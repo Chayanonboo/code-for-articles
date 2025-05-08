50438eecfeb7d7452ca3f5bf0875c6a28ac7d093
Chayanon Boonthanom <166270677+Chayanonboo@users.noreply.github.com>
2025-05-08 13:37:50 +0700

    Update Introduction to SQL.sql

--- SQL/Introduction to SQL.sql
+++ SQL/Introduction to SQL.sql
@@ -1,5 +1,14 @@
-## SELECT All Book
+https://chayanonboo.com/2024/12/18/5-steps-with-project-introduction-sql/
+
+## Selecting all fields
 SELECT * FROM books;
 
 ## SELECT column name from patron
 SELECT name FROM patrons;
+
+## Selecting multiple fields
+SELECT card_num, name FROM patrons;
+
+## Alasing name of SQL
+SELECT name AS firstname, year_hired FROM employees;
+
